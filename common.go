37e909fa7e337fc3630e68e8f9cc2ce138544c93
Matt <mt76@gmx.com>
2021-04-22 17:16:30 +0100

    Update common.go

--- common.go
+++ common.go
@@ -1,8 +1,10 @@
 package common
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
+	"runtime"
 	"strconv"
 	"time"
 )
@@ -19,4 +21,11 @@ func Snooze() {
 	rand.Seed(time.Now().UnixNano())
 	n := rand.Intn(10) // n will be between 0 and 10
 	SnoozeFor(strconv.Itoa(n))
+}
+
+// GetCurrentFuncName will return the current function's name.
+// It can be used for a better log debug system.(I'm NOT sure.)
+func GetCurrentFuncName() string {
+	pc, _, _, _ := runtime.Caller(1)
+	return fmt.Sprintf("%s", runtime.FuncForPC(pc).Name())
 }
