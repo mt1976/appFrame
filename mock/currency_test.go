4bc8ddc238c33cdc06c7bad77ea295550a7842d4
Matt <mt76@gmx.com>
2023-08-02 18:51:33 +0100

    Minor Update

--- mock/currency_test.go
+++ mock/currency_test.go
@@ -17,6 +17,7 @@ func TestGetCurrencyInfo(t *testing.T) {
 	}{
 		{"Valid Currency Code", args{"EUR"}, CurrencyInfo{Code: "EUR", SpotDays: 2, Name: "Euro", Character: "€", DPS: 2, QuoteDPS: 4, Type: Currency, MajorUnit: "Euro", MinorUnit: "Cent", ISONumericCode: "978", KnownAs: "Euros", MinorCharacter: "c", YearOfIntroduction: 1999}, false},
 		{"Invalid Currency Code", args{"XX"}, CurrencyInfo{}, true},
+		{"Sterling", args{"GBP"}, CurrencyInfo{Code: "GBP", SpotDays: 2, Name: "Pound Sterling", Character: "£", DPS: 2, QuoteDPS: 4, Type: Currency, MajorUnit: "Pound", MinorUnit: "Pence", ISONumericCode: "826", KnownAs: "Pounds", MinorCharacter: "p", YearOfIntroduction: 800}, false},
 		{"Valid Currency Code", args{"USD"}, CurrencyInfo{Code: "USD", SpotDays: 1, Name: "US Dollar", Character: "$", DPS: 2, QuoteDPS: 4, Type: Currency, MajorUnit: "Dollar", MinorUnit: "Cent", ISONumericCode: "840", KnownAs: "Bucks", MinorCharacter: "c", YearOfIntroduction: 1792}, false},
 		{"Crypto DASH", args{"DASH"}, CurrencyInfo{Code: "DASH", SpotDays: 0, Name: "Dash", Character: "Đ", DPS: 8, QuoteDPS: 8, Type: Crypto, MajorUnit: "Dash", MinorUnit: "Duffs", ISONumericCode: "1005", KnownAs: "Dash", MinorCharacter: "d", YearOfIntroduction: 2014}, false},
 		{"Metal XAU", args{"XAU"}, CurrencyInfo{Code: "XAU", SpotDays: 2, Name: "Gold", Character: "Au", DPS: 0, QuoteDPS: 0, Type: Metals, MajorUnit: "Ounce", MinorUnit: "Ounce", ISONumericCode: "959", KnownAs: "Gold"}, false},
