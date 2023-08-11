563b72492188755dc428c359d6a8c2d8c555b033
Matt <mt76@gmx.com>
2023-08-11 17:33:24 +0100

    Add new translation functionality
    Effective Merge of Config and Translate functionality (may be fully merged in future)

--- translate/api.go
+++ translate/api.go
@@ -2,8 +2,8 @@ package translate
 
 // Get returns a translated string or the original string if no translation is found
 // The Get function returns the result of the get function when given a string input.
-func Get(in string) string {
-	return get(in)
+func Get(property string) string {
+	return get(property, "")
 }
 
 // Verbose turns on verbose logging
@@ -18,8 +18,114 @@ func Normal() {
 	setVerbose(false)
 }
 
-// GetValue returns translated string for a given key and name
-// The GetValue function returns the value associated with a given key and name.
-func GetValue(key string, name string) string {
-	return get(key + "$" + name)
+// GetValue returns translated string for a given property and name
+// The GetValue function returns the value associated with a given property and name.
+func GetValue(property string, category string) string {
+	return get(property, category)
+}
+
+// The GetString function returns a string value for a given property.
+func GetString(property string) (string, error) {
+	return get(property, ""), nil
+}
+
+// The GetStringofCategory function returns a string value based on the given property and category.
+func GetStringofCategory(property string, category string) (string, error) {
+	return get(property, category), nil
+}
+
+// The GetInt function returns an integer value for a given property.
+func GetInt(property string) (int, error) {
+	return getInt(property, "")
+}
+
+// The function GetIntofCategory retrieves an integer value based on a given property and category.
+func GetIntofCategory(property string, category string) (int, error) {
+	return getInt(property, category)
+}
+
+// The GetBool function returns a boolean value based on a given property.
+func GetBool(property string) (bool, error) {
+	return getBool(property, "")
+}
+
+// The function GetBoolofCategory returns a boolean value based on the given property and category.
+func GetBoolofCategory(property string, category string) (bool, error) {
+	return getBool(property, category)
+}
+
+// The GetFloat function retrieves a float value for a given property.
+func GetFloat(property string) (float64, error) {
+	return getFloat(property, "")
+}
+
+// The GetFloatofCategory function retrieves a float value based on a given property and category.
+func GetFloatofCategory(property string, category string) (float64, error) {
+	return getFloat(property, category)
+}
+
+// The GetList function returns a list of strings based on a given property.
+func GetList(property string) ([]string, error) {
+	return getList(property, "")
+}
+
+// The function GetListofCategory returns a list of strings based on the given property and category.
+func GetListofCategory(property string, category string) ([]string, error) {
+	return getList(property, category)
+}
+
+// The function GetListString returns a list of strings based on a given property.
+func GetListString(property string) ([]string, error) {
+	return getList(property, "")
+}
+
+// The function GetListStringofCategory retrieves a list of strings based on a given property and
+// category.
+func GetListStringofCategory(property string, category string) ([]string, error) {
+	return getList(property, category)
+}
+
+// The function GetListInt returns a list of integers and an error based on a given property.
+func GetListInt(property string) ([]int, error) {
+	return getListInt(property, "")
+}
+
+// The function GetListIntofCategory returns a list of integers based on the given property and
+// category.
+func GetListIntofCategory(property string, category string) ([]int, error) {
+	return getListInt(property, category)
+}
+
+// The function GetListBool returns a list of boolean values and an error.
+func GetListBool(property string) ([]bool, error) {
+	return getListBool(property, "")
+}
+
+// The function GetListBoolofCategory returns a list of boolean values based on the given property and
+// category.
+func GetListBoolofCategory(property string, category string) ([]bool, error) {
+	return getListBool(property, category)
+}
+
+// The function GetListFloat returns a list of float64 values and an error, based on a given property.
+func GetListFloat(property string) ([]float64, error) {
+	return getListFloat(property, "")
+}
+
+// The function GetListFloatofCategory returns a list of float64 values based on the given property and
+// category.
+func GetListFloatofCategory(property string, category string) ([]float64, error) {
+	return getListFloat(property, category)
+}
+
+// The GetMap function returns a map of string key-value pairs and an error, based on the provided
+// property.
+func GetMap(property string) (map[string]string, error) {
+	return getMap(property, "")
+}
+
+// The function GetMapofCategory returns a map of strings and an error based on the given property and
+// category.
+func GetMapofCategory(property string, category string) (map[string]string, error) {
+	return getMap(property, category)
 }
--- translate/translate.go
+++ translate/translate.go
@@ -1,6 +1,8 @@
 package translate
 
 import (
+	"errors"
+	"strconv"
 	"strings"
 
 	xio "github.com/mt1976/appFrame/fileio"
@@ -25,10 +27,19 @@ func init() {
 }
 
 // Get returns a translated string or the original string if no translation is found
-func get(in string) string {
+func get(in string, kind string) string {
 	//log.Info("TextGet: ", in)
 	//log.Info("TextGet: ", lowerFirst(in)+"TXT")
-	search := strings.ToLower(in)
+	if in == "" {
+		if verboseLogging() {
+			xlogs.WithFields(xlogger.Fields{"using": in, "kind": kind}).Warn("Nothing to find")
+			//log.Println("TextGet: No Translation for ", in)
+		}
+
+		return ""
+	}
+
+	search := strings.ToLower(setSearch(in, kind))
 	search = strings.ReplaceAll(search, " ", "")
 	//out := str.LowerFirst(in) + "TODO"
 	out := in
@@ -94,4 +105,135 @@ func test() {
 	xlogs.Warning("test")
 	xlogs.WithFields(xlogger.Fields{"test": "test"}).Warning("test")
 	xlogs.Println("test")
+}
+
+// The setSearch function concatenates the property and category strings with a "$" separator if the
+// category is not empty.
+func setSearch(property string, category string) string {
+	if category == "" {
+		return property
+	}
+	if property == "" {
+		return category
+	}
+	return property + "$" + category
+}
+
+// The function `getInt` converts a string value retrieved from a specific property and category into
+// an integer, returning the integer value and any error encountered during the conversion.
+func getInt(property string, category string) (int, error) {
+	rtn, err := strconv.Atoi(get(property, category))
+	if err != nil {
+		return 0, err
+	}
+	return rtn, nil
+}
+
+// The function "getBool" parses a string value retrieved from a property and category, and returns a
+// boolean value along with any error encountered during parsing.
+func getBool(property string, category string) (bool, error) {
+	rtn, err := strconv.ParseBool(get(property, category))
+	if err != nil {
+		return false, err
+	}
+	return rtn, nil
+}
+
+// The function "getFloat" parses a string value retrieved from a specific property and category into a
+// float64 value.
+func getFloat(property string, category string) (float64, error) {
+	rtn, err := strconv.ParseFloat(get(property, category), 64)
+	if err != nil {
+		return 0, err
+	}
+	return rtn, nil
+}
+
+// The getList function takes in a property and category as parameters, retrieves a base string using
+// the get function, splits the base string by commas, and returns the resulting list of strings.
+func getList(property string, category string) ([]string, error) {
+	if property == "" && category == "" {
+		return nil, errors.New("property and category cannot both be empty")
+	}
+	if property == "" {
+		return nil, errors.New("property cannot be empty")
+	}
+	base := get(property, category)
+	if base == property {
+		return nil, errors.New("property not found")
+	}
+	rtn := strings.Split(base, ",")
+	return rtn, nil
+}
+
+// The function `getListInt` takes a property and category as input, retrieves a string value based on
+// the property and category, splits the string into a slice of strings, converts each string element
+// to an integer, and returns a slice of integers along with any error that occurred during the
+// conversion.
+func getListInt(property string, category string) ([]int, error) {
+	base := get(property, category)
+	if base == property {
+		return nil, errors.New("property not found")
+	}
+	rtn := strings.Split(base, ",")
+	var rtn2 []int
+	for _, v := range rtn {
+		rtn3, err := strconv.Atoi(v)
+		if err != nil {
+			return nil, err
+		}
+		rtn2 = append(rtn2, rtn3)
+	}
+	return rtn2, nil
+}
+
+// The getListBool function takes a property and category as input, retrieves a comma-separated string
+// value from a source, converts each value to a boolean, and returns a slice of booleans along with
+// any error encountered.
+func getListBool(property string, category string) ([]bool, error) {
+	base := get(property, category)
+	rtn := strings.Split(base, ",")
+	var rtn2 []bool
+	for _, v := range rtn {
+		rtn3, err := strconv.ParseBool(v)
+		if err != nil {
+			return nil, err
+		}
+		rtn2 = append(rtn2, rtn3)
+	}
+	return rtn2, nil
+}
+
+// The function `getListFloat` takes a property and category as input, retrieves a comma-separated
+// string value from a source, converts each value to a float64, and returns a slice of float64 values
+// along with any error encountered during the conversion process.
+func getListFloat(property string, category string) ([]float64, error) {
+	base := get(property, category)
+	if base == property {
+		return nil, errors.New("property not found")
+	}
+	rtn := strings.Split(base, ",")
+	var rtn2 []float64
+	for _, v := range rtn {
+		rtn3, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return nil, err
+		}
+		rtn2 = append(rtn2, rtn3)
+	}
+	return rtn2, nil
+}
+
+// The function "getMap" takes a property and category as input, retrieves a base string using the
+// "get" function, and returns a map of key-value pairs parsed from the base string.
+func getMap(property string, category string) (map[string]string, error) {
+	base := get(property, category)
+	if base == property {
+		return nil, errors.New("property not found")
+	}
+	rtn := make(map[string]string)
+	for _, v := range strings.Split(base, ",") {
+		rtn[strings.Split(v, ":")[0]] = strings.Split(v, ":")[1]
+	}
+	return rtn, nil
 }
